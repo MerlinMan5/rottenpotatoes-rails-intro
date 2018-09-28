59263305a58e4c54f0b7902b08cbd1be87ed6e06
MerlinMan5 <jonathanearauco@gmail.com>
2018-09-27 22:50:27 -0500

    parts 1-3 complete

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,12 @@ Rails.application.routes.draw do
   root 'movies#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  #resources :products
 
   resources :movies
   
